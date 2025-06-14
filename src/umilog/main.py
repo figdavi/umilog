e31fc52bf013328a9a6f08dad814bcb33238cd5c
figdavi <figueiredodavi.dev@proton.me>
2025-06-14 11:18:50 -0300

    Fix normalized soil moisture value

--- src/umilog/main.py
+++ src/umilog/main.py
@@ -7,13 +7,16 @@ app = FastAPI()
 class LogData(BaseModel):
     soil_moisture: int
 
+def map_range(x: int, in_max: int, out_max: int) -> int:
+  return x * out_max // in_max
+
 @app.post("/log/")
 def log(log_data: LogData):
     file_test = "sensor_data/output.txt"
 
     cur_time = datetime.now().replace(microsecond=0).isoformat()
     
-    normalized_soil_moisture = log_data.soil_moisture / 100
+    normalized_soil_moisture = map_range(log_data.soil_moisture, 1023, 100) / 100
     
     with open(file_test, "a+") as f:
         f.write(f"soil moisture percentage at {cur_time} is {normalized_soil_moisture}\n")
